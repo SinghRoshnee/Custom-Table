9e44dabfe785b4ee89675aa77137672dac996826
SinghRoshnee <singhroshni012001@gmail.com>
2025-02-07 20:55:26 +0530

    updated

--- javascript/main.js
+++ javascript/main.js
@@ -1,7 +1,9 @@
 import { rendertask } from "./rendertask.js";
 import { items } from "./items.js";
 import { additem } from "./additem.js";
+import { search } from "./search.js";
 
 rendertask();
 console.log(items);
-additem()
+additem();
+search();
---
+++ javascript/search.js
@@ -0,0 +1,10 @@
+import { items } from "./items.js";
+import { rendertask } from "./rendertask.js";
+
+const searchBar = document.querySelector("#searchBar");
+export function search() {
+    console.log(searchBar);
+    
+}
+
+//check searchbar is
